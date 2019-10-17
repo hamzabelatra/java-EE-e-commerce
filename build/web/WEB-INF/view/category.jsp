f47c005e0437e2bf76de2829276ddd1de59fa45f
Hamza Belatra <hamzabelatra1@gmail.com>
2019-10-17 19:11:49 +0100

    Handling Session Data adding now you can view the cart add items update ...

--- build/web/WEB-INF/view/category.jsp
+++ build/web/WEB-INF/view/category.jsp
@@ -15,59 +15,65 @@
     <sql:param value="${pageContext.request.queryString}"/>
 </sql:query>--%>
 <div id="categoryLeftColumn">
+
     <c:forEach var="category" items="${categories}">
+
         <c:choose>
-            <c:when test="${category.id==pageContext.request.queryString}">
+            <c:when test="${category.name == selectedCategory.name}">
                 <div class="categoryButton" id="selectedCategory">
-                    <span class="categoryText">${category.name}</span>
+                    <span class="categoryText">
+                        ${category.name}
+                    </span>
                 </div>
             </c:when>
             <c:otherwise>
                 <a href="category?${category.id}" class="categoryButton">
-                    <div class="categoryText">
+                    <span class="categoryText">
                         ${category.name}
-                    </div>
+                    </span>
                 </a>
             </c:otherwise>
         </c:choose>
 
     </c:forEach>
+
 </div>
 
 <div id="categoryRightColumn">
-    <p id="categoryTitle">
-        <span style="background-color: #f5eabe; padding: 7px; ">${selectedCategory.name}</span></p>
 
-  <table id="productTable">
+    <p id="categoryTitle">${selectedCategory.name}</p>
 
-    <c:forEach var="product" items="${categoryProducts}" varStatus="iter">
+    <table id="productTable">
 
-        <tr class="${((iter.index % 2) == 0) ? 'lightBlue' : 'white'}">
-            <td>
-                <img src="${initParam.productimagePath}${product.name}.png"
-                    alt="${product.name}">
-            </td>
-            <td>
-                ${product.name}
-                <br>
-                <span class="smallText">${product.description}</span>
-            </td>
-            <td>
-                &euro; ${product.price} / unit
-            </td>
-            <td>
-                <form action="addToCart" method="post">
-                    <input type="hidden"
-                           name="productId"
-                           value="${product.id}">
-                    <input type="submit"
-                           value="add to cart">
-                </form>
-            </td>
-        </tr>
+        <c:forEach var="product" items="${categoryProducts}" varStatus="iter">
 
-    </c:forEach>
+            <tr class="${((iter.index % 2) == 0) ? 'lightBlue' : 'white'}">
+                <td>
+                    <img src="${initParam.productimagePath}${product.name}.png"
+                         alt="${product.name}">
+                </td>
 
-</table>
+                <td>
+                    ${product.name}
+                    <br>
+                    <span class="smallText">${product.description}</span>
+                </td>
+
+                <td>&euro; ${product.price}</td>
+
+                <td>
+                    <form action="addToCart" method="post">
+                        <input type="hidden"
+                               name="productId"
+                               value="${product.id}">
+                        <input type="submit"
+                               name="submit"
+                               value="add to cart">
+                    </form>
+                </td>
+            </tr>
+
+        </c:forEach>
+
+    </table>
 </div>
-
--- src/java/Controller/ControllerServlet.java
+++ src/java/Controller/ControllerServlet.java
@@ -5,17 +5,21 @@
  */
 package Controller;
 
+import cart.ShoppingCart;
 import entity.Category;
 import entity.Product;
 import java.io.IOException;
 import java.util.Collection;
 import javax.ejb.EJB;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import session.CategoryFacade;
+import session.ProductFacade;
 
 /**
  *
@@ -35,16 +39,25 @@ public class ControllerServlet extends HttpServlet {
 
 @EJB
 private CategoryFacade categoryFacade;
+private String surcharge;
+@EJB
+private ProductFacade productFacade;
 
 @Override
-public void  init() throws ServletException
+public void  init(ServletConfig servletConfig) throws ServletException
 {
+    
+    super.init(servletConfig);
+    surcharge = servletConfig.getServletContext().getInitParameter("deliverySurcharge");
     //store category list in the servlet    
         getServletContext().setAttribute("categories", categoryFacade.findAll());
 }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        HttpSession  session = request.getSession();
+         Category selectedCategory;
+         Collection<Product> categoryProducts;
         String userPath= request.getServletPath();
         if(userPath.equals("/category"))
         {
@@ -54,23 +67,36 @@ public void  init() throws ServletException
             {
                 
                 //get the selected category
-                Category selectedCategory = categoryFacade.find(Short.parseShort(categoryId));
-                request.setAttribute("selectedCategory", selectedCategory);
+                 selectedCategory = categoryFacade.find(Short.parseShort(categoryId));
+                session.setAttribute("selectedCategory", selectedCategory);
                 
                 //get products of selected categories
-                Collection<Product> categoryproducts = selectedCategory.getProductCollection();
+                 categoryProducts = selectedCategory.getProductCollection();
                 //place category product in the request scope 
-                request.setAttribute("categoryProducts", categoryproducts);
+                session.setAttribute("categoryProducts", categoryProducts);
             }
         }
-        else if (userPath.equals("/viewCart"))
-        {
-            userPath="/cart";
-        }
+             else if (userPath.equals("/viewCart")) {
+
+            String clear = request.getParameter("clear");
+
+            if ((clear != null) && clear.equals("true")) {
+
+                ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
+                cart.clear();
+            }
+
+            userPath = "/cart";
+
+  }
+    
         
         else if (userPath.equals("/checkout"))
         {
-            //implement checkout page request
+            ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
+
+            // calculate total
+            cart.calculateTotal(surcharge);
         }
         else if (userPath.equals("chooseLanguage"))
         {
@@ -93,14 +119,37 @@ public void  init() throws ServletException
             throws ServletException, IOException {
         
                 String userPath= request.getServletPath();
-
+                HttpSession session= request.getSession();
+                ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
                 if (userPath.equals("/addToCart"))
                 {
-                    //implement add product to cart
+                     //if user is adding item to the cart for the first time
+                    //create cart object and attach it to user session
+                    if (cart ==null)
+                    {
+                        cart= new ShoppingCart();
+                        session.setAttribute("cart", cart);
+                    }
+                    //get user input from request
+                    String productId = request.getParameter("productId");
+                    if (!productId.isEmpty())
+                    {
+                        Product product =   productFacade.find(Integer.parseInt(productId));
+                        cart.addItem(product);
+                    }
+                    userPath="/category";   
                 }
                 if (userPath.equals("/updateCart"))
                 {
-                    //implement update cart action
+                // get input from request
+            String productId = request.getParameter("productId");
+            String quantity = request.getParameter("quantity");
+
+            Product product = productFacade.find(Integer.parseInt(productId));
+            cart.update(product, quantity);
+
+            userPath = "/cart";
+
                 }
                 if(userPath.equals("/purchase"))
                 {
